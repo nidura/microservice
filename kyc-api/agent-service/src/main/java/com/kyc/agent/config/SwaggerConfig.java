b592ffd6dd162df25ec46c8de87351115d3cb0a7
Nidura Prageeth <niduraprageeth@gmail.com>
2018-10-20 22:57:11 +0530

    swagger ui added

---
+++ kyc-api/agent-service/src/main/java/com/kyc/agent/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.kyc.agent.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/**
+ * Created by Nidura Prageeth
+ * Date : 2018-10-20
+ */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.kyc.agent"))
+                .paths(regex("/.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Dashboard Service - Swagger Rest API Documentation",
+                "Spring Boot - kyc api",
+                "1.0",
+                "Terms of Service",
+                new Contact("Nidura Prageeth", "https://www.linkedin.com/in/nidura-prageeth-bb1404101/",
+                        "niduraprageeth@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html"
+        );
+        return apiInfo;
+    }
+}
